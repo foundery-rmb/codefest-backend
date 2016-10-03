81c3aab88ca72e473b51e37b2ebf53883d40049a
Daclin Woods <degasw@gmail.com>
2016-10-03 15:52:29 +0200

    new version

---
+++ config/config.js
@@ -0,0 +1,37 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'codefestbackend'
+    },
+    port: process.env.PORT || 3000,
+    db: 'sqlite://localhost/codefestbackend-development',
+    storage: rootPath + '/data/codefestbackend-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'codefestbackend'
+    },
+    port: process.env.PORT || 3000,
+    db: 'sqlite://localhost/codefestbackend-test',
+    storage: rootPath + '/data/codefestbackend-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'codefestbackend'
+    },
+    port: process.env.PORT || 3000,
+    db: 'sqlite://localhost/codefestbackend-production',
+    storage: rootPath + 'data/codefestbackend-production'
+  }
+};
+
+module.exports = config[env];
