5ad54978bd90d5323663c61ae8d4acec37c1a118
Daclin Woods <degasw@gmail.com>
2016-10-04 09:21:50 +0200

    added call to luis

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,6 +2,10 @@ var express = require('express'),
   router = express.Router(),
   db = require('../models');
 
+ var request = require('request'); 
+
+ var LUIS_URL =  "https://api.projectoxford.ai/luis/v1/application?id=afad2289-d126-489a-8d9b-2f9f8cf02bdb&subscription-key=b5ab91160f864fde9626ae6b395b35e4&q=";
+
 module.exports = function (app) {
   app.use('/', router);
 };
@@ -17,4 +21,12 @@ router.get('/', function (req, res, next) {
 
 router.get('/ping', function (req, res) {
   res.send('PONG');
+});
+
+router.get('/query/:querytext', function (req, res) {
+  request(LUIS_URL + req.params.querytext, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    	res.send(body);
+  	}
+  })
 });
